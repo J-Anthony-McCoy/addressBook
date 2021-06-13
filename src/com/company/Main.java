3894128999bf42702b89d6e6b001cc93efbb1242
Anthony McCoy <james.anthony.mccoy@gmail.com>
2021-06-12 19:45:30 -0500

    Got no. 2 working. Need to complete search method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Main {
 
-    //      ArrayList to store objects
     static ArrayList<Entry> userEntry = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -13,7 +13,7 @@ public class Main {
     }
 
     public static void welcomePage() {
-//      Scanner to take in user's selection
+
         Scanner input = new Scanner(System.in);
 
 //      Prints options for user to choose
@@ -46,82 +46,107 @@ public class Main {
 //        }
         if (option == 1) {
             addEntry();
-        } else if (option == 2){
+        } else if (option == 2) {
             removeEntry();
-        }else if (option == 4) {
+        } else if (option == 4) {
             printAddressBook();
-        }else if (option == 5){
+        } else if (option == 5) {
             deleteAddressBook();
-        }
-        else if ( option == 6) {
+        } else if (option == 6) {
             quitProgram();
-        }
-        else {
+        } else {
             System.out.println("Still coding!");
         }
-        }
+    }
 
-//      Method for adding a new entry
-        public static void addEntry() {
+//      No. 1 - Method to add a new entry
+    public static void addEntry() {
 
-//      Scanner to take in user entries
-            Scanner entryInput = new Scanner(System.in);
+        Scanner entryInput = new Scanner(System.in);
 
-            System.out.println("\n***** Adding a New Entry *****");
-            System.out.println("Please add your entry. All fields are required.");
-            System.out.println("First Name: ");
-            String firstName = entryInput.nextLine();
-            System.out.println("Last Name: ");
-            String lastName = entryInput.nextLine();
-            System.out.println("Phone Number: ");
-            String phoneNumber = entryInput.nextLine();
-            System.out.println("Email Address: ");
-            String emailAddress = entryInput.nextLine();
-            System.out.println("New entry is added!");
+        System.out.println("\n***** Adding a New Entry *****");
+        System.out.println("Please add your entry. All fields are required.");
+        System.out.println("First Name: ");
+        String firstName = entryInput.nextLine();
+        System.out.println("Last Name: ");
+        String lastName = entryInput.nextLine();
+        System.out.println("Phone Number: ");
+        String phoneNumber = entryInput.nextLine();
+        System.out.println("Email Address: ");
+        String emailAddress = entryInput.nextLine();
+        System.out.println("New entry is added!");
 
 //      Object to store all user entry data
-            Entry entry = new Entry(firstName, lastName, phoneNumber, emailAddress);
-            entry.setFirstName(firstName);
-            entry.setLastName(lastName);
-            entry.setPhoneNumber(phoneNumber);
-            entry.setEmailAddress(emailAddress);
+        Entry entry = new Entry(firstName, lastName, phoneNumber, emailAddress);
+        entry.setFirstName(firstName);
+        entry.setLastName(lastName);
+        entry.setPhoneNumber(phoneNumber);
+        entry.setEmailAddress(emailAddress);
 
 //      Add last entry object into array list
-            userEntry.add(entry);
+        userEntry.add(entry);
 
-//      Back to welcome page
-            welcomePage();
-        }
-
-//      Method to remove an entry based off email input by user
-        public static void removeEntry(){
+        welcomePage();
+    }
+//      No. 2 - Method to remove an entry
+    public static void removeEntry() {
         Scanner input = new Scanner(System.in);
-            System.out.println("\n***** Removing an Entry *****");
-            System.out.println("Enter the email for the entry you want to remove: ");
-            String emailToRemove = input.nextLine();
-
-
-
+        System.out.println("\n***** Removing an Entry *****");
+        System.out.println("Enter the email for the entry you want to remove: ");
+        String emailToRemove = input.nextLine();
+        for (int i = 0; i < userEntry.size(); i++) {
+            if (userEntry.get(i).getEmailAddress().equals(emailToRemove)) {
+                userEntry.remove(i);
+                System.out.println("Entry is deleted!");
+                welcomePage();
+            }
         }
+    }
+//            } else if (!userEntry.get(i).getEmailAddress().equals(emailToRemove)){
+//                System.out.println("Entry not found...Sending you back to Welcome Page.");
+//                welcomePage();
+//            }
+//                System.out.println("Entry found: " + userEntry.get(i) + "\nIs this the entry you want to remove? Type Y or N.");
+//                String confirmDelete = input.nextLine();
+//                if (confirmDelete.equals("Y") || confirmDelete.equals("y") || confirmDelete.equals("Yes") || confirmDelete.equals("yes")) {
+//                    userEntry.remove(i);
+//                    System.out.println("Entry deleted!");
+//                    welcomePage();
+//                } else if (confirmDelete.equals("N") || confirmDelete.equals("n") || confirmDelete.equals("No") || confirmDelete.equals("no")) {
+//                    System.out.println("No entry will be deleted.");
+//                    welcomePage();
+//                }
 
-//      Method to print entire address book
-        public static void printAddressBook(){
-            System.out.println(userEntry);
-            welcomePage();
-        }
+//      No. 3 - Method to search for an entry
+    public static void searchEntry(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("How do you want to search for an entry? Please enter a number 1 - 4.\n");
+        System.out.println("1) First name");
+        System.out.println("2) Last name");
+        System.out.println("3) Phone Number");
+        System.out.println("4) Email Address");
+        int searchMethod = input.nextInt();
 
-//      Method to delete entire address book
-        public static void deleteAddressBook(){
-            userEntry.clear();
-            System.out.println("\n***** Deleting the Entire Address Book *****");
-            System.out.println("All data in Address Book is deleted!");
-            welcomePage();
-        }
+    }
 
-//      Method to quit the program
-        public static void quitProgram() {
-            System.out.println("Thanks! See you next time.");
-        }
+//      No. 4 - Method to print entire address book
+    public static void printAddressBook() {
+        System.out.println(userEntry);
+        welcomePage();
+    }
+
+//      No. 5 - Method to delete entire address book
+    public static void deleteAddressBook() {
+        userEntry.clear();
+        System.out.println("\n***** Deleting the Entire Address Book *****");
+        System.out.println("All data in Address Book is deleted!");
+        welcomePage();
+    }
+
+//      No. 6 - Method to quit the program
+    public static void quitProgram() {
+        System.out.println("Thanks! See you next time.");
+    }
 
 }
 
